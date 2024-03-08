20be17b0bf5c52eacb1016916613cf2d48e6689b
Νεκτάριος Παπακωνσταντινόπουλος <npapakwn@hotmail.com>
2024-03-08 20:56:20 +0200

    Parse Server + Dashboard Ready

--- backend/index.js
+++ backend/index.js
@@ -4,25 +4,31 @@ import path from 'path';
 import dotenv from 'dotenv';
 const __dirname = path.resolve();
 import http from 'http';
-import fs from 'fs';
+import ParseDashboard from 'parse-dashboard';
 
+// Resolving the path for the environment variables file and configuring dotenv to use the specified environment variables file 
 const envFilePath = path.resolve(__dirname, '.env');
-console.log('Loading .env file from:', envFilePath);
-
 dotenv.config({ path: envFilePath });
-console.log('dotenv configuration result:', dotenv.config());
 
+// Configuration settings for Parse Server
 export const config = {
+  // MongoDB connection URI
   databaseURI: process.env.DB_URI,
-  appId: process.env.APP_ID || 'defaultAppId', // Provide a default value if not set
-  masterKey: process.env.MASTER_KEY || 'defaultMasterKey', // Provide a default value if not set
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // Provide a default value if not set
+  // Parse Server application ID with a default value
+  appId: process.env.APP_ID || 'defaultAppId',
+  // Parse Server master key with a default value
+  masterKey: process.env.MASTER_KEY || 'defaultMasterKey',
+  // Parse Server URL with a default value
+  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',
+  // Port for the server to listen on
   port: process.env.SERVER_PORT,
 };
-export const app = express();
 
+export const app = express();
+// Serve static files from the 'public' directory
 app.use('/public', express.static(path.join(__dirname, '/public')));
 
+// Asynchronous IIFE for starting Parse Server
 (async () => {
   try {
     const server = new ParseServer(config);
@@ -32,6 +38,25 @@ app.use('/public', express.static(path.join(__dirname, '/public')));
     console.error('Error starting Parse Server:', error);
   }
 
+  // Create Parse Dashboard instance with configuration settings
+  var dashboard = new ParseDashboard({
+    "apps": [
+      {
+        "serverURL": process.env.SERVER_URL,
+        "appId": process.env.APP_ID,
+        "masterKey": process.env.MASTER_KEY,
+        "appName": process.env.APP_NAME
+      }
+    ],
+    "users": [
+      {
+        "user": process.env.APP_USER,
+        "pass": process.env.APP_PASS
+      },]
+  });
+
+    // Mount Parse Dashboard at the '/dashboard' endpoint of the Express app
+    app.use('/dashboard', dashboard);
 
   if (!process.env.TESTING) {
     const port = process.env.SERVER_PORT || 1337; // Provide a default value if not set
